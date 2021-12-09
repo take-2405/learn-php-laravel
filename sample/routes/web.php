6056760097bdf3a2a692348bd2361071d3f6b430
take-2405 <take12.wa@icloud.com>
2021-12-09 10:02:43 +0900

    add 新規ページ、元ページを問題なく表示

--- sample/routes/web.php
+++ sample/routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('new-page');
+    return view('welcome');
 });
 
-// Route::get('/new', function () {
-//     return view('new-page');
-// });
+Route::get('/new', function () {
+    return view('new-page');
+});
